cad476b099e596c9f96b25e245a1c2e9a8c0909a
Rohit Pal <rohit03993@gmail.com>
2025-09-18 10:58:10 +0530

    Add automatic cache-busting for CSS files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,8 @@
     <link rel="shortcut icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link href="{{ asset('css/paytm-theme.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/notifications.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/paytm-theme.css') }}?v={{ filemtime(public_path('css/paytm-theme.css')) }}" rel="stylesheet">
+    <link href="{{ asset('css/notifications.css') }}?v={{ filemtime(public_path('css/notifications.css')) }}" rel="stylesheet">
     <style>
         /* Mobile-first responsive design */
         body {
